603bfc9ecc2fb6c899c65f47746c57234ebeffbe
yazanmo <yazanmostafa96@gmail.com>
2021-05-15 00:57:46 +0300

    git all articles

--- main.js
+++ main.js
@@ -6,6 +6,36 @@ const port = 5000;
 app.use(express.json());
 
 
+const articles = [
+    {
+    id: 1,
+    title: 'How I learn coding?',
+    description:
+    'Lorem, Quam, mollitia.',
+    author: 'Jouza',
+    },
+    {
+    id: 2,
+    title: 'Coding Best Practices',
+    description:
+    'Lorem, ipsum dolor sit, Quam, mollitia.',
+    author: 'Besslan',
+    },
+    {
+    id: 3,
+    title: 'Debugging',
+    description:
+    'Lorem, Quam, mollitia.',
+    author: 'Jouza',
+    },
+];
+
+
+app.get("/articles" , (req , res , next )=> {
+    res.status(200);
+    res.json(articles)
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
