c8bddd76a1958a91a056f8737c934154ba92266b
yazanmo <yazanmostafa96@gmail.com>
2021-05-21 22:35:29 +0300

    Update Authentication middleware done

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 const express = require("express");
-const uuid  = require("uuidv4");
 const axios = require("axios");
 const bcrypt = require("bcrypt");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const { uuid } = require("uuidv4");
+const db = require("./db");
+const { users, articles,comments,roles } = require("./schema");
 
-const db = require("./db")
-
-const {users,articles,comments,roles} = require("./schema")
 
 const app = express();
 const port = 5000;
 app.use(express.json());
 
 const SECRET = process.env.SECRET
+
+
+
 
 
 app.post("/users",(req,res)=>{
@@ -25,100 +27,113 @@ app.post("/users",(req,res)=>{
   }).catch((err) => {
     res.send(err);
   });
-})
+});
 
 
-app.post("/articles",(req,res)=>{
+
+
+
+app.post("/articles",  (req,res)=>{
   const {title,description,author} = req.body
-  const {titledescription} = req.body
-    const newArticle = new articles({title,description,author})
-  
-      newArticle.save()
-      .then((result)=>{
-        res.status(201)
-        res.json(result)
-      }).catch((err) => {
-        res.send(err);
-      });
-  
-  });
-
-  app.post("/addRole",(req,res)=>{
-    const {role,permissions} = req.body
-    const newRole = new roles({role,permissions})
-    newRole.save()
-      .then((result)=>{
-        res.status(201)
-        res.json(result)
-      }).catch((err) => {
-        res.send(err);
-      });
-  });
-
-
-
-
-  app.post("/login",(req,res,next)=>{
-    const {email,password} = req.body;
-    users.findOne({email:email}).populate("roles").then((response)=>{
-      if (response){
-      const hashedPassword = response.password
-        bcrypt.compare(password, hashedPassword, (err, result) => {
-          if (result){
-            const payload = {
-              userId: `${response._id}`,
-              country: response.country,
-              role:{role:response.roles.role,permissions:response.roles.permissions}
-            };
-            const  options =  { expiresIn: '2h' }
-            const token = jwt.sign(payload, SECRET, options);
-  
-  
-            res.status(200)
-            res.json({token})
-          }else{
-            const err = new Error("The password is incorrect");
-            err.status = 404;
-            next(err);
-          }
-        });
-      }else{
-      const err = new Error("The email doesn't exist");
-      err.status = 404;
-      next(err);
-    };
-    }).catch((err) => {
-      res.send(err);
-    });
- 
-  
-
-
-
-  app.get("/articles", (req,res)=>{
-    articles.find({}).populate("comments","comment").populate("author","firstName")
-    .then(result=>{
-      res.status(200)
+  const newArticle = new articles({title,description,author})
+    newArticle.save()
+    .then((result)=>{
+      res.status(201)
       res.json(result)
     }).catch((err) => {
       res.send(err);
     });
-  });
+});
 
 
 
-  
-  app.get("/articles/search_1", async (req,res)=>{
-  
-    const userId = req.query.author;
-    articles.find({author:userId}).then((result)=>{
-      res.status(200);
+
+
+app.post("/addRole",(req,res)=>{
+  const {role,permissions} = req.body
+  const newRole = new roles({role,permissions})
+  newRole.save()
+    .then((result)=>{
+      res.status(201)
       res.json(result)
     }).catch((err) => {
       res.send(err);
-    }); 
-  
+    });
+});
+
+
+
+
+
+
+app.post("/login",(req,res,next)=>{
+  const {email,password} = req.body;
+  users.findOne({email:email}).populate("roles").then((response)=>{
+    if (response){
+    const hashedPassword = response.password
+      bcrypt.compare(password, hashedPassword, (err, result) => {
+        if (result){
+          
+          const payload = {
+            userId: `${response._id}`,
+            country: response.country,
+            role: { role:response.roles.role, permissions:response.roles.permissions }
+          };
+          const  options =  { expiresIn: '60m' }
+          const token = jwt.sign(payload, SECRET, options);
+          res.status(200)
+          res.json({
+            token
+          })
+        }else{
+          const err = new Error("The password you’ve entered is incorrect");
+          err.status = 403;
+          next(err);
+        }
+      });
+    }else{
+    const err = new Error("The email doesn't exist");
+    err.status = 404;
+    next(err);
+  };
+  }).catch((err) => {
+    res.send(err);
   });
+});
+
+
+
+
+
+app.get("/articles", (req,res)=>{
+  articles.find({}).populate("comments","comment").populate("author","firstName")
+  .then(result=>{
+    res.status(200)
+    res.json(result)
+  }).catch((err) => {
+    res.send(err);
+  });
+});
+
+
+
+
+
+
+app.get("/articles/search_1",  (req,res)=>{
+
+  const userId = req.query.author;
+  articles.find({author:userId}).then((result)=>{
+    res.status(200);
+    res.json(result)
+  }).catch((err) => {
+    res.send(err);
+  }); 
+
+});
+
+
+
 
 
 app.get("/articles/search_2",(req,res)=>{
@@ -133,11 +148,14 @@ app.get("/articles/search_2",(req,res)=>{
 });
 
 
+
+
+
 app.put("/articles/:id",(req,res)=>{
   const id = req.params.id;
   const {title,description,author} = req.body
   if (req.body.title && req.body.description && req.body.author){
-  articles.findByIdAndUpdate(id,{title,description,author},{new:true})
+  articles.findByIdAndUpdate(id,{title,description,author}, {new:true})
   .then((result)=>{
     res.status(200)
     res.json(result)
@@ -146,23 +164,31 @@ app.put("/articles/:id",(req,res)=>{
   });
  }else{
   res.status(404);
-  res.json(" please enter title and  description ");
+  res.json("must enter title and  description ");
  }
 });
 
-  
+
+
+
+
+
 app.delete("/articles/:id",(req,res)=>{
   const id = req.params.id;
   articles.findByIdAndDelete(id).then((result)=>{
     res.status(200)
     res.json({
       success: true,
-      massage: ` Delete article with id Successfull => ${id}`,
+      massage: `Delete article with id Successfull => ${id}`,
     })
   }).catch((err) => {
     res.send(err);
   });
 });
+
+
+
+
 
 app.delete("/articles",(req,res)=>{
   const author = req.body.author;
@@ -176,59 +202,64 @@ app.delete("/articles",(req,res)=>{
   }).catch((err) => {
     res.send(err);
   });
+
 });
 
+
+
+
 const authentication = (req,res,next)=>{
+  if (!req.headers.authorization){
+    res.status(404)
+    return res.json("no token")
+  }
   const token = req.headers.authorization.split(" ")[1];
-  console.log(token)
-
-  jwt.verify(token, SECRET, (err, result) => {
-    if (err) {
-      res.status(403)
-      return res.json({
-        message : "the token is expired",
-        status: 403
-      });
-    }
-    if (result) {
-      console.log(result)
-      next();
-    }
-  });
-
-
-
-
-
-
-  app.post("/articles/:id/comments",authentication,(req,res)=>{
-    const id = req.params.id
-    const {comment,commenter} = req.body
-    const newComment = new comments({comment,commenter})
-    newComment.save().then(async (result)=>{
-     await articles.updateOne(
-      { _id: id }, 
-      { $push: { comments: result._id } }
-  );
-      res.status(201)
-      res.json(result)
-    }).catch((err) => {
-      res.send(err);
-    });
-  });
-
   
+  try {
+    const varif = jwt.verify(token, SECRET)
+    if (varif) {
+    req.token = "verified token"
+    next();
+    }
+  }  
+  catch (err){
+    res.status(403)
+    return res.json({
+      message : "the token is expired",
+      status: 403
+    })
+  }
+};
 
-  app.use((err, req, res, next) => {
-    res.status(err.status);
-    res.json({
-      error: {
-        status: err.status,
-        message: err.message,
-      },
-    });
+app.post("/articles/:id/comments",authentication,(req,res)=>{
+  const id = req.params.id
+  const {comment,commenter} = req.body
+  const newComment = new comments({comment,commenter})
+  newComment.save().then(async (result)=>{
+   await articles.updateOne(
+    { _id: id }, 
+    { $push: { comments: result._id } }
+);
+    res.status(201)
+    res.json(result)
+  }).catch((err) => {
+    res.send(err);
   });
+});
+
+app.use((err, req, res, next) => {
   
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-  })
+  res.status(err.status);
+  
+  res.json({
+    error: {
+      status: err.status,
+      message: err.message,
+    },
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
